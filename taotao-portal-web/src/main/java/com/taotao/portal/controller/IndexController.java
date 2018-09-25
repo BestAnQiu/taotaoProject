3a78246df26ce3213acfa00359fde3a03758f818
BestAnQiu <1135938458@qq.com>
2018-09-25 19:23:57 +0800

    首页表现层

---
+++ taotao-portal-web/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.taotao.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	/**
+	 * 首页展示
+	 * @return
+	 */
+	/*@RequestMapping(value="/index")
+	public String showIndex() {
+		return "index";
+	}*/
+}
